27b5f44e3798e41a00ae28756b5694043aef2fe4
Timilehin-bello <emmatimmybello@gmail.com>
2022-11-13 21:13:25 +0100

    Create Navbar component and add Discover, Help center and Notification component with onclick functionaity

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -18,5 +18,98 @@ export const NavBar = () => {
   const [profile, setProfile] = useState(false);
   const [openSideMenu, setOpenSideMenu] = useState(false);
 
-  return <div>NavBar</div>;
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if (btnText === "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if (btnText === "Help Center") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(true);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(true);
+    }
+  };
+
+  const openNotification = () => {
+    if (!notification) {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  };
+
+  return (
+    <div className={Style.navBar}>
+      <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={images.logo}
+              alt="Cyclone NFT Marketpace"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search" />
+              <BsSearch onClick={(e) => {}} className={Style.search_icon} />
+            </div>
+          </div>
+        </div>
+
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            <p
+              onClick={(e) => {
+                openMenu(e);
+              }}
+            >
+              Discover
+            </p>
+            {discover && (
+              <div className={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+          </div>
+          <div className={Style.navbar_container_right_help}>
+            <p
+              onClick={(e) => {
+                openMenu(e);
+              }}
+            >
+              Help Center
+            </p>
+            {help && (
+              <div className={Style.navbar_container_right_help_box}>
+                <HelpCenter />
+              </div>
+            )}
+          </div>
+
+          <div className={Style.navbar_container_right_notify}>
+            <MdNotifications
+              className={Style.notify}
+              onClick={() => {
+                openNotification();
+              }}
+            />
+            {notification && <Notification />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
